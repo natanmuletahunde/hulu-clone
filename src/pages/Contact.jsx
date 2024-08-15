305d6e9898959896975b3c9b55b0811e7b12d7ec
pivnati <natanmuleta911@gmail.com>
2024-08-14 22:38:42 -0700

    adding the contact file in the pages folder

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Contact Us</h1>
+      <p>Here you can contact us for more information.</p>
+    </div>
+  );
+};
+
+export default Contact;
