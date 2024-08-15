a1b520fd16ad9a64342b158b951c5797ff45d6f2
pivnati <natanmuleta911@gmail.com>
2024-08-14 22:05:57 -0700

    in the header file remove the import logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import logo from '../assets/logo.png'; // Import the image file
 
 const Header = () => {
   return (
     <header className="bg-black p-4">
-      <img src={logo} alt="Hulu Logo" className="w-24" /> {/* Use the logo variable directly */}
+      <img src="/assets/logo.png" alt="Hulu Logo" className="w-24" />
     </header>
   );
 };
