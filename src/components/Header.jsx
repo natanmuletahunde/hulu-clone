1c55d0b687d1ece7514a0a68d85780fa8c861498
pivnati <natanmuleta911@gmail.com>
2024-08-14 22:00:48 -0700

    use the log directly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.png'; // Import the image file
 
 const Header = () => {
   return (
     <header className="bg-black p-4">
-      <img src={logo} alt="Hulu Logo" className="w-24" />
+      <img src={logo} alt="Hulu Logo" className="w-24" /> {/* Use the logo variable directly */}
     </header>
   );
 };
