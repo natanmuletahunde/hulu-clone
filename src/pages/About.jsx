b2bd1ccdc9562b06653aa83ece0071285268dc3e
pivnati <natanmuleta911@gmail.com>
2024-08-14 22:36:36 -0700

    adding the about file in the pages folder

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">About Us</h1>
+      <p>Welcome to the About page. Here you can learn more about us.</p>
+    </div>
+  );
+};
+
+export default About;
