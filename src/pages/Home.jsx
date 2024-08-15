66c4d36a69fd529394231c96e3cf1c79c7b99554
pivnati <natanmuleta911@gmail.com>
2024-08-14 21:53:34 -0700

    delete the dummy.js file from the assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { movies } from '../assets/dummy-data';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import NavBar from '../components/NavBar';
 import MovieCard from '../components/MovieCard';
 import Footer from '../components/Footer';
+
+const placeholderMovies = [
+  { id: 1, title: "Sample Movie 1", image: "path/to/sample-image1.jpg", description: "Description for Sample Movie 1" },
+  { id: 2, title: "Sample Movie 2", image: "path/to/sample-image2.jpg", description: "Description for Sample Movie 2" },
+  // Add more placeholder movies if needed
+];
 
 const Home = () => {
   return (
@@ -14,7 +19,7 @@ const Home = () => {
       <NavBar />
       <Banner />
       <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {movies.map(movie => (
+        {placeholderMovies.map(movie => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
